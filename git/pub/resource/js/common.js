64c4b4a7204590043eea3add1a993ab31d9543ef
diaxoxo-dev <diaxoxo@naver.com>
2021-04-14 16:15:11 +0900

    Revert "오류수정 및 코드추가"
    This reverts commit 836f1c97a741b16db35eb31ca11670b05c5decce.

--- git/pub/resource/js/common.js
+++ git/pub/resource/js/common.js
@@ -3,31 +3,13 @@ window.commonJs = window.commonJs || {}
 var commonJs = function(){
 	var common = {
 			init : function(){
-				common.deviceOpt();
-				common.diviceSize();
+				common.autoGnb();
 			},
-			deviceOpt : function(){
-				if (navigator.userAgent.indexOf('iPhone') != -1) {  
-						// 주소창 숨기기 아이폰인 경우  
-						addEventListener("load", function() {        
-						setTimeout(hideURLbar, 0);}, false);
-						// 주소창 숨기기 기기 분기 클래스 추가
-						$("body").addClass("ios");
-					} 
-					else {     
-					//아이폰이 아닌 경우
-					hideURLbar();
-					// 주소창 숨기기 기기 분기 클래스 추가
-					$("body").addClass("android");
-					}
-				function hideURLbar(){ 
-					window.scrollTo(0, 1);
-				}
-			},
-			diviceSize : function($deviceHeight){
-				var $deviceHeight = $(window).height(); // 장치높이
-				var $contH = document.body.clientHeight; // 컨텐츠 높이 
-				$(".wrap").height($deviceHeight);
+			autoGnb : function(){
+				$(".gnb__all").on("click" , function(event){
+					event.preventDefault();
+					$(this).siblings(".nav").toggleClass("active");
+				});
 			}
 		};
 		return common;
